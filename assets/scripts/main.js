3b455b2446cb00a40e54a56cbb693cacbf33f4c3
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2023-01-27 20:37:53 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+// Sticky Navigation Bar
+
+window.onscroll = function() {stickyNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
